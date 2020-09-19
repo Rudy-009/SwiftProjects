653497788745810765eeb1219bb2db65412dd5aa
Rudy-009 <rofxnaos@gmail.com>
2020-09-19 17:57:16 +0900

    first playground file

---
+++ MyPlayground01.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+var j = 0;
+
+for i in 0...3 {
+    j+=i
+}
---
+++ MyPlayground01.playground/Sources/Additional.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public let globalConstant = 100;
+//This is comment
+public func publicFunction() {
+    print("Hello")
+}
